00087a2ef34547b2478314606d85b8d9edbb9b91
D-h-i-v-y-a <70471036+D-h-i-v-y-a@users.noreply.github.com>
2021-03-16 13:57:39 +0530

    Navbar

---
+++ navbar.php
@@ -0,0 +1,29 @@
+<html>
+  
+<link rel="stylesheet" type="text/css" href="css/navbar.css">  
+
+<nav class="navbar navbar-expand-md navbar-black bg-light">
+      
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      
+
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php"><b>Home</b></a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php"><b>View Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
+       </html>
+
+       
+       
