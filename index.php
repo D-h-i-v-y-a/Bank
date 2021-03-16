61e3c2874909178fd7daccda8e5ecef278da5975
D-h-i-v-y-a <70471036+D-h-i-v-y-a@users.noreply.github.com>
2021-03-16 13:55:33 +0530

    Home Page

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>Home Page</title>
+  <style>
+    #footer
+    {
+      padding:20px 20px 0px 20px;
+      position:fixed;
+      bottom:0;
+      width:100%;
+      height:50px;
+      background:white;
+      text-align:center;
+    }
+  body
+  {
+    background-image: url('img/piggy.jpg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;  
+    background-size: cover;
+  }
+</style>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  <div class="container-fluid">
+    <div class="heading text-center my-5 font-size-40px">
+        <b> <h1>SPARKS BANK</h1></b>
+                </div>
+              </div>
+            
+<div class="row activity text-center">
+           <div class="col-md-12 act">
+               <p> <b><h2>The habit of saving is itself an education; it fosters every virtue, teaches self-denial, cultivates the sense of order, trains to forethought, and so broadens the mind.</h2></b></p>
+          </div>
+</div>
+
+      
+      </body>
+      </html>
+      <div id="footer"><center>Made by Dhivya.S of The Sparks Foundation</center></div>
+      
+  
+
+
+
+
